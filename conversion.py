c53d7d53ba56940e6e10c7f59bfee02e2baa324b
Ethan Tan <ectan@college.harvard.edu>
2024-07-02 18:38:10 -0400

    working tests

---
+++ conversion.py
@@ -0,0 +1,4 @@
+from docx2pdf import convert
+import os
+
+convert(f'docx projects/', f'pdf projects/')
---
+++ conversion_test.py
@@ -0,0 +1,7 @@
+from docx2pdf import convert
+
+# successfully works for docx
+# convert("conversion_test/1490.docx", "conversion_test/1490.pdf")
+
+# does it work for doc?
+convert("conversion_test/1490.doc", "conversion_test/1490.pdf")
